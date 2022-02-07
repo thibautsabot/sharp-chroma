e1b11cd2b1e3fa5d0cc6e7feb996c5bb2c72677e
Thibaut Sabot <thibaut.sabot@adevinta.com>
2022-02-07 18:18:41 +0100

    add both transformed images

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ import fs from "fs";
 import sharp from "sharp";
 
 async function transform() {
-  const upstreamBuffer = Buffer.from(fs.readFileSync("./big_picture.jpg"));
+  const upstreamBuffer = Buffer.from(fs.readFileSync("./picture.png"));
   const transformer = sharp(upstreamBuffer);
 
   transformer.rotate().avif({
     quality: 70,
-    chromaSubsampling: "4:4:4",
+    chromaSubsampling: "4:2:0",
   });
 
   const optimizedBuffer = await transformer.toBuffer();
 
-  fs.promises.writeFile("transformed_big.avif", optimizedBuffer);
+  fs.promises.writeFile("transformed-4.2.0.avif", optimizedBuffer);
 }
 
 transform();
